b91bbab3a196dccf19b0f743d5e61c604e22e34b
Aditya Wanjari <aditya@Adityas-MacBook-Pro.local>
2025-09-16 23:15:05 -0400

    added team instructions

--- team.py
+++ team.py
@@ -1,19 +1,22 @@
 from agno.agent import Agent
 from agno.models.openai import OpenAIChat
-from agno.storage.sqlite import SqliteStorage
+# from agno.storage.sqlite import SqliteStorage
 from agno.team import Team
-from agno.tools.duckduckgo import DuckDuckGoTools
-from agno.tools.hackernews import HackerNewsTools
+# from agno.tools.duckduckgo import DuckDuckGoTools
+# from agno.tools.hackernews import HackerNewsTools
 from agno.utils.pprint import pprint_run_response
-from agno.workflow.v2.types import WorkflowExecutionInput
-from agno.workflow.v2.workflow import Workflow
+# from agno.workflow.v2.types import WorkflowExecutionInput
+# from agno.workflow.v2.workflow import Workflow
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os, json
 import prompts
-from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
+from agno.knowledge.knowledge import Knowledge
+from agno.knowledge.reader.pdf_reader import PDFReader
+from agno.vectordb.pgvector import PgVector, SearchType
+
 from agno.vectordb.pgvector import PgVector
-from agno.embedder.openai import OpenAIEmbedder
+from agno.knowledge.embedder.openai import OpenAIEmbedder
 
 load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
@@ -27,47 +30,82 @@ class AgentResponse(BaseModel):
 class GradeResponse(BaseModel):
     grade : int = Field(..., description="Student Grade")
 
-pdf_knowledge_base = PDFKnowledgeBase(
-    path="Data/southwest/southwest_case_study.pdf",
-    # Table name: ai.pdf_documents
-    vector_db=PgVector(
-        table_name="pdf_documents",
-        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
-        embedder=OpenAIEmbedder(id="text-embedding-3-large"),
 
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+knowledge = Knowledge(
+    # Use PgVector as the vector database and store embeddings in the `ai.recipes` table
+    vector_db=PgVector(
+        table_name="pdf_docs",
+        db_url=db_url,
+        search_type=SearchType.hybrid,
+        embedder=OpenAIEmbedder(id="text-embedding-3-large"),
     ),
-    reader=PDFReader(chunk=True),
 )
+
+knowledge.add_content(
+    path="Data/southwest/southwest_case_study.pdf",
+    reader=PDFReader()
+
+)
+# pdf_knowledge_base = Knowledge(
+#     path="Data/southwest/southwest_case_study.pdf",
+#     # Table name: ai.pdf_documents
+#     vector_db=PgVector(
+#         table_name="pdf_documents",
+#         db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+#         embedder=OpenAIEmbedder(id="text-embedding-3-large"),
+
+#     ),
+#     reader=PDFReader(chunk=True),
+# )
 
 #  Defining Agents
 
 rubric_agent1 = Agent(
     model = OpenAIChat(id = "gpt-4o", api_key = API_KEY),
     name = "Industry Analysis Agent",
-    instructions = prompts.CRITERIA1_PROMPT,
-    response_model = AgentResponse,
+    role = prompts.CRITERIA1_PROMPT,
+    output_schema = AgentResponse,
     )
 
 rubric_agent2 = Agent(
     model = OpenAIChat(id = "gpt-4o", api_key = API_KEY),
     name = "Comparison Agent",
-    instructions = prompts.CRITERIA2_PROMPT,
-    response_model = AgentResponse,
+    role = prompts.CRITERIA2_PROMPT,
+    output_schema = AgentResponse,
     )
 
 rubric_agent3 = Agent(
     model = OpenAIChat(id = "gpt-4o", api_key = API_KEY),
     name = "RAG Agent",
-    knowledge=pdf_knowledge_base,
+    knowledge=knowledge,
     search_knowledge=True,
-    instructions = prompts.CRITERIA3_PROMPT,
-    response_model = AgentResponse,
+    role = prompts.CRITERIA3_PROMPT,
+    output_schema = AgentResponse,
     )
-rubric_agent3.knowledge.load(recreate=False)
+# rubric_agent3.knowledge.load(recreate=False)
 
 rubric_agent4 = Agent(
     model = OpenAIChat(id = "gpt-4o", api_key = API_KEY),
     name = "Presentation Agent",
-    instructions = prompts.CRITERIA4_PROMPT,
-    response_model = AgentResponse,
+    role = prompts.CRITERIA4_PROMPT,
+    output_schema = AgentResponse,
     )
+
+
+team = Team(
+    name = "Evaluator Team",
+    model = OpenAIChat(id = "gpt-4o", api_key = API_KEY),
+    members = [rubric_agent1,rubric_agent2,rubric_agent3,rubric_agent4],
+    determine_input_for_members=False,
+    instructions=[
+        "You are a professor teaching Business Analytics, your job is to evaluate student assignments",
+        f"Here is the question: {prompts.TEST_QUESTION}",
+        "Provide the student response to all 4 agents and submit their outputs to the user"
+    ],
+    show_members_responses=True,
+
+)
+
+team.print_response(input=prompts.TEST_RESPONSE)
